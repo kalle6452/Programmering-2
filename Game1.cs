b5503d833706369c568b89c380705d35f8230080
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2021-09-13 14:28:46 +0200

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -61,3 +61,49 @@ spriteBatch.Begin();
         }
     }
 }
+
+
+
+using System;
+
+namespace projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
+
+
+
+class Triangel : Shape
+{
+    public area {
+        return width * height;
+    }
+    public Triangel(int width, int height): base(width, height)
+    {
+
+    }
+}
+/*{
+   public int Area{get;}
+   public int Circumferance {get;}
+}*/
+
+
+class Shape
+{
+    public int Height{get; set;}
+    public int Width{get; set;}
+
+public Shape(int width, int height)
+{
+    height = Height;
+    width = Width;
+}
+}
