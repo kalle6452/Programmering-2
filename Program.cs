0fa9bb0f0c832f172d575d09e144a032ebed420b
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2021-09-27 14:28:04 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace PersonElevAnställd
     {
         static void Main(string[] args)
         {
-          List<Person> plist = new List<Person>();
-            plist.Add(new Anställd("Nisse", "700101-5600",25000));
+         
+      
+          
+         /*   plist.Add(new Anställd("Nisse", "700101-5600",25000));
             plist.Add(new Elev("Kajsa", "20010506-2600",2019,"Teknik"));
             Console.Write("Ange namn: ");
             string namn=Console.ReadLine();
@@ -17,33 +19,54 @@ namespace PersonElevAnställd
             int lön=int.Parse(Console.ReadLine());
            string allt = namn + personnr + lön;
            string elevinput = namn + personnr;
-            Console.WriteLine(allt);
+            Console.WriteLine(allt);*/
          
-            foreach(var item in plist)
+          /*  foreach(var item in plist)
             {
                 Console.WriteLine(item);
             }
-    
+  */
+              int menyval = -1;
+          while (menyval != 0)
+          {
+            List<Person> plist = new List<Person>();
+               Console.WriteLine("1 lägg till elev.");
+               Console.WriteLine("2 lägg till anställd");
+               Console.WriteLine("3 skriv upp alla anställda.");
+               Console.WriteLine("4 skriv upp alla elever.");
+               Console.WriteLine("5 Avsluta programmet");
+               int meny = int.Parse(Console.ReadLine());
+               if (meny==2)//Varför 2 ==
+               {
+                 
+                  Console.Write("Ange namn: ");
+            string namn=Console.ReadLine();
+              Console.Write("Ange personnummer: ");
+            string personnr=Console.ReadLine();
+              Console.Write("Ange lön: ");
+                  int lön=int.Parse(Console.ReadLine());
+              Console.WriteLine("Ange utbildningsprogram ");
+            string program=Console.ReadLine();
+               }
+               if(meny==1)
+               {
+                     Console.Write("Ange namn: ");
+            string namn=Console.ReadLine();
+              Console.Write("Ange personnummer: ");
+            string personnr=Console.ReadLine();
+            Console.Write("Ange antagningsår ");
+            int år=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ange Utbildningsprogram");
+            string program=Console.ReadLine();
+               }
+               if(meny==3)
+               {
+                  foreach(var item in plist)
+            {
+                Console.WriteLine(item);
+            }
+               }
+          }
         }
     }
 }
-//Abstrakt klass, får ej skapa instanser av, kan däremot ärva från, person ska vara det, person är aldrig bara person antingen elev eller personal.
-//Koncept
-//Referensvariabel
-//case, när något specifikt händer som inte kan klassas som något annat.
-/*
-    if (Console.KeyAvailable)
-        {
-            var key = Console.ReadKey(true);
-            switch (key.Key)
-            {
-                case ConsoleKey.UpArrow:
-            }
-            if (Console.CursorTop > 0)
-            {
-                Console.Write("");
-            }
-            
-        }*/
-
-//https://stackoverflow.com/questions/45063659/user-object-input-into-list-of-data/45063898
