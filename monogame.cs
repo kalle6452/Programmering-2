38ad765587be0c6e80920ac35b2ae5885069a7d5
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2021-10-11 14:27:36 +0200

    Create monogame.cs

---
+++ monogame.cs
@@ -0,0 +1,78 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Sprite {
+    private Texture2D texture {get;set;}
+    public Vector2 position{get;set;}
+    public Rectangle hitbox{get;set;}
+    public Sprite(Texture2D texture, Vector2 position, Rectangle hitbox)
+    {
+        this.texture = texture;
+        this.position = position;
+        this.hitbox = hitbox;
+    }
+}
+
+
+
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace monogameSprite
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D xwing;
+        Vector2 xwingPos;
+        Rectangle xwingRect;
+        private Sprite sprite1;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            sprite1 = new Sprite(Content.Load<Texture2D>("xwing"));
+            xwingPos=new Vector2(150,250);
+            xwingRect=new Rectangle((int)xwingPos.X, (int)xwingPos.Y, 200, 200);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            xwing=Content.Load<Texture2D>("xwing");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || 
+                    Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(xwing,xwingRect,Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
